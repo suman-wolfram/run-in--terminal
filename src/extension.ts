68da8ac8edc8bad54c5b26e9ae70df098bf01ebf
Suman Banerjee <sumanb@wolfram.com>
2024-08-08 17:49:12 +0530

    Update.

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,26 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {
+	commands,
+	window
+} from 'vscode';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "run-in-terminal" is now active!');
+	context.subscriptions.push(
+		commands.registerCommand(
+			'run-in-terminal.runInTerminal', () => {
+					
+					const wolframscriptTerminal = window.createTerminal(`WolframKernel`, "/Applications/Wolfram 2.app/Contents/MacOS/WolframKernel");
+					wolframscriptTerminal.show();
+					window.showInformationMessage('WolframKernel Launched');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('run-in-terminal.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from run-in-terminal!');
-	});
-
-	context.subscriptions.push(disposable);
+			}
+		)
+	);
 }
 
 // This method is called when your extension is deactivated
